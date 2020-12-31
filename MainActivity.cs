1e42d786fdbfddbba12208a9624c8fbab6e9f1c1
sl s <630746464@qq.com>
2020-12-31 11:06:18 +0800

    添加UDP

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,7 +3,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
-
+using Custom_Communiations;
 namespace Raffle_Andriod
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
@@ -16,8 +16,27 @@ namespace Raffle_Andriod
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
+            UDP udp = new UDP();
+            Enthernet e = new Enthernet();
+            udp.UDP_Open(e.GetLocalIp(),11066);
 
+            Button start = FindViewById<Button>(Resource.Id.start);
+            start.Click += (sender, e) =>
+            {
+                udp.UDP_Write("start", "255.255.255.255", 11066);//向UDP发送“start”
+            };
+            Button stop = FindViewById<Button>(Resource.Id.stop);
+            stop.Click += (sender, e) =>
+            {
+                udp.UDP_Write("stop", "255.255.255.255", 11066); //向UDP发送“stop”
+            };
+            Button restart = FindViewById<Button>(Resource.Id.restart);
+            restart.Click += (sender, e) =>
+            {
+                udp.UDP_Write("restart", "255.255.255.255", 11066);//向UDP发送“restart”
+            };
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
